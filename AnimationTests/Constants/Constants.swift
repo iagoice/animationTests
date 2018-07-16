48074af99170cac5767b5cfd78696d7715ddfad7
Iago Mordente <iagoc@ciandt.com>
2018-07-16 16:56:15 -0300

    Adicionado o menu

---
+++ AnimationTests/Constants/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  AnimationTests
+//
+//  Created by Iago Mordente Rezende Leão Corrêa on 16/07/18.
+//  Copyright © 2018 Iago Mordente Rezende Leão Corrêa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let options = ["Draw shapes"]
+    
+    static let zero: CGFloat = 0
+    
+    struct Texts {
+        static let optionsTitle = "Selecione a animação"
+    }
+    
+    struct Sizes {
+        static let optionsHeaderHeight: CGFloat = 50
+        static let optionsCellHeight: CGFloat = 30
+    }
+    
+    struct Identifiers {
+        static let optionsCell = "optionsCellIdentifier"
+    }
+    
+}
---
+++ AnimationTests/Controller/OptionsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  AnimationTests
+//
+//  Created by Iago Mordente Rezende Leão Corrêa on 16/07/18.
+//  Copyright © 2018 Iago Mordente Rezende Leão Corrêa. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+
+    @IBOutlet weak var optionsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        optionsTableView.dataSource = self
+        optionsTableView.delegate = self
+        optionsTableView.tableFooterView = UIView()
+    }
+}
+
+extension OptionsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerFrame = CGRect(x: Constants.zero, y: Constants.zero, width: tableView.frame.width, height: Constants.Sizes.optionsHeaderHeight)
+        let headerView = UIView(frame: headerFrame)
+        
+        let labelFrame = CGRect(x: headerFrame.minX + 20, y: headerFrame.midY - headerFrame.height/2, width: tableView.frame.width, height: Constants.Sizes.optionsHeaderHeight)
+        let titleLabel = UILabel(frame: labelFrame)
+        titleLabel.text = Constants.Texts.optionsTitle
+        titleLabel.textAlignment = .center
+        
+        headerView.addSubview(titleLabel)
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return Constants.Sizes.optionsHeaderHeight
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return Constants.Sizes.optionsCellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Constants.options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: Constants.Identifiers.optionsCell)
+        let labelFrame = CGRect(x: cell.frame.minX, y: cell.frame.midY - cell.frame.height/2, width: tableView.frame.width, height: Constants.Sizes.optionsCellHeight)
+        let label = UILabel(frame: labelFrame)
+        label.text = Constants.options[indexPath.row]
+        cell.addSubview(label)
+        return cell
+    }
+}
+
+extension OptionsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
